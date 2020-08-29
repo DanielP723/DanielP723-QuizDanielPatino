314a5f3397627a2f11615a240f4668ee7ffd2701
Daniel Patiño <daniel.patino.2020@upb.edu.co>
2020-08-28 23:59:20 -0500

    Único commit,no me sirvió la extensión de visual studio

---
+++ Home.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_DanielPatiño
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        private void cbGaseosas_Checked(object sender, RoutedEventArgs e)
+        {
+            cmbProductos.Items.Add("Gaseosa de Manzana");
+            cmbProductos.Items.Add("Gaseosa de Naranja");
+            cmbProductos.Items.Add("Gaseosa de Uva");
+            cmbProductos.Items.Add("Gaseosa de Piña");
+        }
+
+        private void cbJugos_Checked(object sender, RoutedEventArgs e)
+        {
+            cmbProductos.Items.Add("Jugo de Mango");
+            cmbProductos.Items.Add("Jugo de Lulo");
+            cmbProductos.Items.Add("Jugo de Mora");
+            cmbProductos.Items.Add("Jugo Tropical");
+        }
+
+        private void cbGaseosas_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cmbProductos.Items.Remove("Gaseosa de Manzana");
+            cmbProductos.Items.Remove("Gaseosa de Naranja");
+            cmbProductos.Items.Remove("Gaseosa de Uva");
+            cmbProductos.Items.Remove("Gaseosa de Piña");
+        }
+
+        private void cbJugos_Unchecked(object sender, RoutedEventArgs e)
+        {
+            cmbProductos.Items.Remove("Jugo de Mango");
+            cmbProductos.Items.Remove("Jugo de Lulo");
+            cmbProductos.Items.Remove("Jugo de Mora");
+            cmbProductos.Items.Remove("Jugo Tropical");
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            lbProductos.Items.Add(cmbProductos.SelectedItem);
+        }
+
+        private void btnRemover_Click(object sender, RoutedEventArgs e)
+        {
+            lbProductos.Items.Remove(lbProductos.SelectedItem);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lblPrecio.Visibility = Visibility.Visible;
+            lblPrecio.Content = "$" + (lbProductos.Items.Count * 2000);
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow cerrar_sesion = (MainWindow)Window.GetWindow(this);
+            cerrar_sesion.frame_Main.NavigationService.Navigate(new Login());
+        }
+    }
+}
---
+++ Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz_DanielPatiño
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "Daniel" && txtPassword.Password == "1234") 
+            {
+                MainWindow iniciar_sesion= (MainWindow)Window.GetWindow(this);
+                iniciar_sesion.frame_Main.NavigationService.Navigate(new Home());
+            }
+            else
+             {
+                MessageBox.Show("Usuario no encontrado");
+                txtUsername.Text = "";
+                txtPassword.Password = "";
+             }
+        }
+    }
+}
